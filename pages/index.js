3a3ea479d6981781022c54ad24e3241105100e64
nika lomashvili <76586954+nikaloamashvili@users.noreply.github.com>
2023-02-16 16:51:04 +0200

    preview

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,13 @@ export default function Home() {
 <meta property="og:url" content="https://metatags.io/"/>
 <meta property="og:title" content="Nika's portfolio"/>
 <meta property="og:description" content="Personal Portfolio Website created using ReactJS"/>
-<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+<meta property="og:image" content="https://ibb.co/mGj6V18"/>
 
 <meta property="twitter:card" content="summary_large_image"/>
 <meta property="twitter:url" content="https://metatags.io/"/>
 <meta property="twitter:title" content="Nika's portfolio"/>
 <meta property="twitter:description" content="Personal Portfolio Website created using ReactJS"/>
-<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
+<meta property="twitter:image" content="https://ibb.co/mGj6V18"></meta>
       </Head>
       <main className={styles.main}>
         <Navbar/>
